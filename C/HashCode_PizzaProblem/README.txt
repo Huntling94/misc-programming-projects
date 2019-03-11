dcd9f5ac83984e06ecbb97b2716381f979600ba0
William Lord <lordwilliam94@gmail.com>
2019-03-12 04:38:12 +1100

    Naive solution completed.

--- C/HashCode_PizzaProblem/README.txt
+++ C/HashCode_PizzaProblem/README.txt
@@ -3,4 +3,17 @@ To compile:
 GCC:
 gcc -Wall -o test pizza.c ..\..\..\C_Code\Utilities\utils.c ..\..\..\C_Code\Files\files.c ..\..\..\C_Code\Matrix\matrix.c ..\..\..\C_Code\Vector\vector.c ..\..\..\C_Code\Math_Extended\math_extended.c
 
-to run, make sure to pass in the filename of the data in as an argument, eg: test example.in
+to run all the files: test example.in small.in medium.in big.in
+
+output files will be generated conforming to the Submission format except for the first line not containing the number of slices (this can be manually inserted).
+
+Score:
+    example: 8
+    small:   34
+    medium:  47,559
+    big:     893,027
+
+    Total:   940628
+    Runtime: ~19 seconds.
+
+The solution is a relatively naive algorithm (clearly the score for example could be improved by inspection).
--- C/HashCode_PizzaProblem/pizza.c
+++ C/HashCode_PizzaProblem/pizza.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <time.h>
 
 #include "..\..\..\C_Code\Utilities\utils.h"
 #include "..\..\..\C_Code\Files\files.h"
@@ -11,8 +12,62 @@
 
 #define TOMATO 1
 #define MUSHROOM 0
+#define SOLUTION "_solution"
 
-matrix_t* read_pizza_in(char* fname, int* pizza_rows, int* pizza_columns, int* min_ingredients, int* max_slice)
+#define MAX_SEARCH_SLICES 20
+typedef struct pizza pizza_t;
+typedef struct slice slice_t;
+
+struct pizza{
+    matrix_t* pizza;
+    char* fname;
+    int rows;
+    int cols;
+    int min_ingredients;
+    int max_slice;
+    int num_tomato;
+    int num_mushroom;
+};
+
+struct slice{
+    int r1;
+    int r2;
+    int c1;
+    int c2;
+};
+
+matrix_t* read_pizza_in(char* fname, int* rows, int* cols, int* min_ingredients, int* max_slice);
+int area_of_slice(int r1, int c1, int r2, int c2);
+int tomatos_in_slice(pizza_t* pizza, int r1, int c1, int r2, int c2);
+int is_valid_slice(pizza_t* pizza, int r1, int c1, int r2, int c2);
+
+pizza_t* create_pizza(char* fname)
+{
+    pizza_t* pizza = malloc(sizeof*pizza);
+    assert(unwanted_null(pizza));
+    pizza->pizza = read_pizza_in(fname, &pizza->rows, &pizza->cols, &pizza->min_ingredients, &pizza->max_slice);
+    pizza->fname = fname;
+    pizza->num_tomato = pizza->pizza->grand_sum(pizza->pizza);
+    pizza->num_mushroom = pizza->rows * pizza->cols - pizza->num_tomato;
+    return pizza;
+}
+
+pizza_t* copy_pizza(pizza_t* pizza)
+{
+    pizza_t* ret = malloc(sizeof*ret);
+    assert(unwanted_null(ret));
+    ret->fname = pizza->fname;
+    ret->rows = pizza->rows;
+    ret->cols = pizza->cols;
+    ret->min_ingredients = pizza->min_ingredients;
+    ret->max_slice = pizza->max_slice;
+    ret->num_tomato = ret->num_tomato;
+    ret->num_mushroom = ret->num_mushroom;
+    ret->pizza = create_matrix(ret->rows, ret->cols);
+    return ret;
+}
+
+matrix_t* read_pizza_in(char* fname, int* rows, int* cols, int* min_ingredients, int* max_slice)
 {
     int lines = lines_in_file(fname);
     char** buffer = malloc(lines * sizeof(*buffer));
@@ -22,21 +77,20 @@ matrix_t* read_pizza_in(char* fname, int* pizza_rows, int* pizza_columns, int* m
 
     /* Extract information from first line of file */
     token = strtok(token, " ");
-    *pizza_rows = atoi(token);
+    *rows = atoi(token);
     token = strtok(NULL, " ");
-    *pizza_columns = atoi(token);
+    *cols = atoi(token);
     token = strtok(NULL, " ");
     *min_ingredients = atoi(token);
     token = strtok(NULL, " ");
     *max_slice = atoi(token);
-    printf("%d %d %d %d\n", *pizza_rows, *pizza_columns, *min_ingredients, *max_slice);
     
-    matrix_t* pizza = create_matrix(*pizza_rows, *pizza_columns); // Store pizza here
+    matrix_t* pizza = create_matrix(*rows, *cols); // Store pizza here
     int i, j;
 
     /* Extract information about pizza from file */
-    for(i=1; i<*pizza_rows+1; i++){
-        for(j=0; j<*pizza_columns; j++){
+    for(i=1; i<*rows+1; i++){
+        for(j=0; j<*cols; j++){
             if (buffer[i][j] == 'T'){
                 pizza->set_entry(pizza, i-1, j, TOMATO);
             }
@@ -49,69 +103,214 @@ matrix_t* read_pizza_in(char* fname, int* pizza_rows, int* pizza_columns, int* m
 
 int area_of_slice(int r1, int c1, int r2, int c2)
 {
-    return ((abs(r1 - r2) + 1) * (abs(c1 - c2) + 1));
+    return ((abs(r1 - r2)+1) * (abs(c1 - c2)+1));
 }
 
-int tomatos_in_slice(matrix_t* pizza, int r1, int c1, int r2, int c2)
+int tomatos_in_slice(pizza_t* pizza, int r1, int c1, int r2, int c2)
 {
     if (pizza == NULL){
         error_set_to_null_message("matrix");
     }
     
     assert(r1 >= 0 && r2 >= 0 && c1 >= 0 && c2 >= 0);
-    assert(r1 <= pizza->num_rows && r2 <= pizza->num_rows
-           && c1 <= pizza->num_columns && c2 <= pizza->num_columns);
+    assert(r1 <= pizza->pizza->num_rows && r2 <= pizza->pizza->num_rows
+           && c1 <= pizza->pizza->num_columns && c2 <= pizza->pizza->num_columns);
     int num =0;
     int i, j;
     int higher_row;
     int lower_row;
     int higher_column;
     int lower_column;
-    (r1 > r2) ? (higher_row = r1) : (higher_row = r2);
+    (r1 > r2) ? (higher_row = r1+1) : (higher_row = r2+1);
     (r1 > r2) ? (lower_row = r2) : (lower_row = r1);
-    (c1 > c2) ? (higher_column = c1) : (higher_column = c2);
+    (c1 > c2) ? (higher_column = c1+1) : (higher_column = c2+1);
     (c1 > c2) ? (lower_column = c2) : (lower_column = c1);
-
-    for(i=lower_row; i<higher_row+1; i++){
-        for(j=lower_column; j<higher_column+1; j++){
-            num += pizza->get_entry(pizza, i, j);
+    for(i=lower_row; i<higher_row; i++){
+        for(j=lower_column; j<higher_column; j++){
+            num += pizza->pizza->get_entry(pizza->pizza, i, j);
         }
     }
     return num;
 }
 
-/*
-void get_all_slices(int* possible_slices, int row, int col, int min_ingredients, int max_slice)
+int is_valid_slice(pizza_t* pizza, int r1, int c1, int r2, int c2)
 {
-    int alloc = 10;
-    int num_slices = 0;
+    if(r1 < 0 || r2 < 0 || c1 < 0 || c2 < 0){
+        return 0;
+    }
+    if(r1 >= pizza->pizza->num_rows || r2 >= pizza->pizza->num_rows
+           || c1 >= pizza->pizza->num_columns || c2 >= pizza->pizza->num_columns){
+        return 0;
+   }
+    int area = area_of_slice(r1, c1, r2, c2);
+    if (area > pizza->max_slice){
+        return 0;
+    }
+    int num_tomato = tomatos_in_slice(pizza, r1, c1, r2, c2);
+    int num_mushroom = area - num_tomato;
+    if (num_tomato < pizza->min_ingredients || num_mushroom < pizza->min_ingredients){
+        return 0;
+    }
+    return 1;
+}
 
+
+
+slice_t* create_slice(int r1, int c1, int r2, int c2)
+{
+    slice_t* slice = malloc(sizeof(*slice));
+    assert(unwanted_null(slice));
+    slice->r1 = r1;
+    slice->r2 = r2;
+    slice->c1 = c1;
+    slice->c2 = c2;
+    return slice;
+}
+
+void print_slice(slice_t* slice)
+{
+    printf("(%d, %d) to (%d, %d)\n", slice->r1, slice->c1, slice->r2, slice->c2);
+}
+
+int get_all_slices(pizza_t* pizza, slice_t** slices, int row, int col, int write_to_file)
+{
     int other_row = row; // Other end of slice
     int other_col = col; // Other end of slice
+
+    int row_count = 1;
+    int col_count = 1;
+    int count = 1;
+    int invalid_repetitions = 0;
+    int slices_found = 0;
     for(;;){
         int i;
-        for(i=0; i<max_slice; i++){
+        for(i=0; i<count; i++){
+            (count%2 == 0) ? (other_col--) : (other_col++);
+            int valid = is_valid_slice(pizza, row, col, other_row, other_col);
+            if(valid){
+                slice_t* slice = create_slice(row, col, other_row, other_col);
+                slices[slices_found++] = slice;
+                if(slices_found == MAX_SEARCH_SLICES){
+                    return slices_found;
+                }
+            }
+            else{
+                invalid_repetitions++;
+            }
+        }
+        col_count++;
+        for(i=0; i<count; i++){
+            (count%2 == 0) ? (other_row--) : (other_row++);
+            int valid = is_valid_slice(pizza, row, col, other_row, other_col);
+            if(valid){
+                slice_t* slice = create_slice(row, col, other_row, other_col);
+                slices[slices_found++] = slice;
+                if(slices_found == MAX_SEARCH_SLICES){
+                    return slices_found;
+                }
+            }
+            else{
+                invalid_repetitions++;
+            }
+        }
+        if(invalid_repetitions > pizza->min_ingredients*((pizza->max_slice*pizza->max_slice))){
+            break;
+        }
+        row_count++;
+        assert(row_count == col_count && row_count == count+1);
+        count++;
+    }
+    return slices_found;
+}
 
+
+int eat_slice(pizza_t* eaten_pizza, slice_t* slice)
+{
+    int higher_row;
+    int lower_row;
+    int higher_column;
+    int lower_column;
+    (slice->r1 > slice->r2) ? (higher_row = slice->r1+1) : (higher_row = slice->r2+1);
+    (slice->r1 > slice->r2) ? (lower_row = slice->r2) : (lower_row = slice->r1);
+    (slice->c1 > slice->c2) ? (higher_column = slice->c1+1) : (higher_column = slice->c2+1);
+    (slice->c1 > slice->c2) ? (lower_column = slice->c2) : (lower_column = slice->c1);
+    int i, j;
+    for(i=lower_row; i<higher_row; i++){
+        for(j=lower_column; j<higher_column; j++){
+            if (eaten_pizza->pizza->get_entry(eaten_pizza->pizza, i, j)){
+                return 0;
+            }
         }
     }
+    for(i=lower_row; i<higher_row; i++){
+        for(j=lower_column; j<higher_column; j++){
+            eaten_pizza->pizza->set_entry(eaten_pizza->pizza, i, j, 1);
+        }
+    }
+    return 1;
 }
-*/
 int main(int argc, char* argv[])
 {
-    int pizza_rows=0, pizza_columns=0, min_ingredients=0, max_slice=0;
-    matrix_t* pizza = read_pizza_in(argv[1], &pizza_rows, &pizza_columns, &min_ingredients, &max_slice);
+    clock_t t;
+    t = clock();
 
-    int num_tomato, num_mushroom;
-    num_tomato = pizza->grand_sum(pizza);
-    num_mushroom = pizza_rows * pizza_columns - num_tomato;
+    int file;
+    int total_score = 0;
+    for(file=1; file<argc; file++){
 
-    //pizza->print(pizza);
-    printf("Number of Mushrooms: %d\n", num_mushroom);
-    printf("Number of Tomatos: %d\n", num_tomato);
+        pizza_t* pizza = create_pizza(argv[file]);
+        printf("Solving: %s, %d x %d\n", argv[file], pizza->rows, pizza->cols);
+        int i, j, k;
 
-    printf("%d\n", area_of_slice(0, 0, 999, 999));
-    printf("Number of tomatos: %d\n", tomatos_in_slice(pizza, 0, 0, 999, 999));
-    //int** possible_slices = malloc(pizza_rows * sizeof(*possible_slices));
-    //matrix_t* eaten_pizza = pizza->copy(pizza);
+        slice_t** temp_slices = malloc(MAX_SEARCH_SLICES * sizeof(*temp_slices));
+        assert(unwanted_null(temp_slices));
 
+        pizza_t* eaten_pizza = copy_pizza(pizza);
+        char* solution_file = malloc(strlen(argv[file])+2+strlen(SOLUTION));
+        for(i=0; i<strlen(argv[file])-3; i++){
+            solution_file[i] = argv[file][i];
+        }
+        for(j=0; j<strlen(SOLUTION); j++){
+            solution_file[i++] = SOLUTION[j];
+        }
+        solution_file[i++] = '.';
+        solution_file[i++] = 't';
+        solution_file[i++] = 'x';
+        solution_file[i++] = 't';
+        solution_file[i] = '\0';
+
+        FILE *fp = fopen(solution_file, "w");
+        int num_slices = 0;
+        int slices_eaten = 0;
+        int eaten = 0;
+        for(i=0; i<pizza->rows; i++){
+            for(j=0; j<pizza->cols; j++){
+                num_slices = get_all_slices(pizza, temp_slices, i, j, 0);
+                if(num_slices){
+                    for(k=0; k<num_slices; k++){
+                        eaten = eat_slice(eaten_pizza, temp_slices[k]);
+                        if(eaten){
+                            slices_eaten++;
+                            fprintf(fp, "%d %d %d %d\n", temp_slices[k]->r1, temp_slices[k]->c1, temp_slices[k]->r2, temp_slices[k]->c2);
+                            break;
+                        }
+                    }
+                }
+                for(k=0; k<num_slices; k++){
+                    free(temp_slices[k]);
+                }
+            }
+        }
+        printf("\tSlices eaten: %d, Score: %lf\n", slices_eaten, eaten_pizza->pizza->grand_sum(eaten_pizza->pizza));
+        total_score += eaten_pizza->pizza->grand_sum(eaten_pizza->pizza);
+    }
+    printf("Total Score across "); printf("%s", argv[1]);
+    int i;
+    for(i=2; i<argc; i++){
+        printf(", %s", argv[i]);
+    }
+    printf(": %d\n", total_score);
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+    printf("Solution took %f seconds to execute \n", time_taken); 
 }
